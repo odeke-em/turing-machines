962cc2720bef80c6f69f259ff456bae197dda9ef
Emmanuel Odeke <odeke@ualberta.ca>
2015-11-03 20:56:40 -0700

    R/W back with infinite length string
    + Using a R/W back and eliminated the reliance
    on integer precision so can now run on a string of infinite length
    only limited by the memory the machine can have and can copy on a
    strncpy.

--- div3.c
+++ div3.c
@@ -11,19 +11,8 @@ typedef enum {
     Unknown=-1, Rejected=0, Accepted=1,
 } State;
 
-size_t bitLength(const size_t v) {
-    if (v <= 1)
-        return 1;
-
-    size_t len = 0; 
-    size_t temp = 1;
-
-    while (temp <= v) {
-        len += 1;
-        temp <<= 1;
-    }
-
-    return len;
+inline int cAtoi(const char c) {
+    return c == '1' ? 1 : 0;
 }
 
 State divisibleByThree(const char *tape) {
@@ -31,35 +20,45 @@ State divisibleByThree(const char *tape) {
     // binary division by means of XOR-ing from the 
     // MSB with 0x3 or 0b11
 
-    // Inclusion of Epsilon the empty string
-    // Should produce an accepting state
+    // Inclusion of Epsilon the empty string -> AcceptingState
     if (tape == NULL)
         return Accepted;
 
-    size_t len = strlen(tape);
-    if (len < 1)
-        return Accepted;
+    // Trim out all the leading 0s
+    size_t i = 0;
+    for (i=0; ;i++) {
+        if (tape[i] == '\0')
+            return Accepted;
 
-    size_t i;
-    long long unsigned int bitfilling = 0;
-
-    // Create the number itself
-    for (i = len; i > 0; i--) {
-        size_t iprecise = i - 1;
-        if (tape[iprecise] != '1')
-            continue;
-
-        bitfilling |= 1 << (len - i);
+        if (cAtoi(tape[i]) != 0)
+            break;
     }
-            
-    while (bitfilling > 1) {
-        int bl = bitLength(bitfilling);
-        int bx = bl - 2;
-        bitfilling ^= (size_t)(0x3 << bx);
+
+    const char *offset = tape + i;
+    size_t len = strlen(offset);
+
+    char *tape_copy = malloc(len);
+    assert(tape_copy != NULL);
+
+    tape_copy = strncpy(tape_copy, offset, len);
+    size_t v, end;
+
+    // Iterate until only one character is left
+    // the value of the last character determines the divisibility
+    for (i = 0, end = len-1; i < end; i++) {
+        v = cAtoi(*tape_copy) << 1;
+        if (v == 0)
+            break;
+
+        tape_copy++;
+        v |= cAtoi(*tape_copy);
+
+        // Write it back to the head
+        *tape_copy = '0' + ((v ^ 0x3) & 0x1);
     }
 
     // No remainder expected if truly divisible by 3
-    return bitfilling == 1 ? Rejected : Accepted;
+    return cAtoi(*tape_copy) == 1 ? Rejected: Accepted;
 }
 
 void printState(State st) {
@@ -71,12 +70,19 @@ int main() {
         char *tape;
         State finalState;
     }testCases[] = {
+            {.tape="11000", .finalState=Accepted},
             {.tape="110", .finalState=Accepted},
             {.tape="0", .finalState=Accepted},
             {.tape="1", .finalState=Rejected},
             {.tape="10", .finalState=Rejected},
             {.tape="1001", .finalState=Accepted},
+            {.tape="10010", .finalState=Accepted},
+            {.tape="00000000000", .finalState=Accepted},
+            {.tape="00000000010", .finalState=Rejected},
+            {.tape="00000000011", .finalState=Accepted},
             {.tape="11000011010011111", .finalState=Accepted},
+            {.tape="1100001101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011111", .finalState=Accepted},
+            {.tape="000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", .finalState=Accepted},
     };
 
     size_t i;
